618bd486e53ebcb2a4213270040c8c1f69558564
14Riteshshet <tgshet777@gmail.com>
2025-12-16 13:20:23 +0530

    Add files via upload

---
+++ app.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Simple application class to demonstrate the Maven build.
+ * This class will be packaged into a .jar file by Maven.
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Jenkins Maven Build Successful!" );
+        System.out.println( "Application version 1.0-SNAPSHOT is running." );
+    }
+}
